2473d02774844f6eed84a0352e1a86e9a3cbff16
Kenny Endersen <120049167+kennyendersen@users.noreply.github.com>
2024-03-11 11:34:54 -0400

    adc lupa

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector('.search-box');
+let text = document.querySelector('.input-buscar');
+let lupa = document.querySelector('.lupa');
+let apagar = document.querySelector('.fechar');
+
+apagar.addEventListener('click', ()=>{
+    text.classList.add('ativar')
+})
